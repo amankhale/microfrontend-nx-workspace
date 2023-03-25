0f2ec796400eb00759c2d8b0d39bce81ee6326ff
amankhale <aman.k@supersourcing.com>
2023-03-25 22:14:40 +0530

    Initial commit

---
+++ apps/details/src/app/remote-entry/nx-welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'nx-workspace-nx-welcome',
+  template: `This is details component`,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent {}
